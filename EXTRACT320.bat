be5e176ed678f5a4df2ed4e38a045c34c1e21e29
Zhiro90 <zhirominoda@gmail.com>
2026-02-19 13:24:46 -0600

    Delete EXTRACT320.bat

--- EXTRACT320.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal ENABLEDELAYEDEXPANSION
-set fName=%1
-set ffmpeg="ffmpeg.exe"
- 
-for /f "tokens=* delims= " %%F in ('echo %fName%') do (
-ffmpeg.exe -i "%%~fF" -ab 320k "%%~dpnF.mp3"
-)
